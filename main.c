1f5a02b2db516182d9b23ff8ae9a3dda6b78c042
Pablo Emidio <p.emidiodev@gmail.com>
2021-12-28 22:37:05 -0300

    Add comments to explain the code based

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+"Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats.
+You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you,
+"Do you want to pick door No. 2?" Is it to your advantage to switch your choice?"
+source: https://en.wikipedia.org/wiki/Monty_Hall_problem
+Based on rep: https://github.com/PabloEmidio/Monty-Hall-problem
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
